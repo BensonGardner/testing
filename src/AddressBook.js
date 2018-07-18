5d0641993135320bc4a405ed83163f86966cfc07
bensongardner <bensongardner@yahoo.com>
2018-07-17 23:14:59 -0500

    feat: Add more specs and code

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,16 +1,33 @@
 function AddressBook() {
-    this.addContact = function() {
-        
-    };
-    this.getContact = function() {
-        
-    };
+    this.contacts = [];
+    this.initialComplete = false;
 };
 
+AddressBook.prototype.addContact = function(contact) {
+        this.contacts.push(contact);
+    };
 
+AddressBook.prototype.getContact = function(index) {
+        return this.contacts[index];
+    };
+
+AddressBook.prototype.deleteContact = function(index) {
+    this.contacts.splice(index, 1);
+};
+
+AddressBook.prototype.getInitialContacts = function(cb) {
+    var self = this;
+    
+    setTimeout(function() {
+        self.initialComplete = true;
+        if (cb) {
+            return cb();
+        }
+    }, 3);
+}
 
 var Contact = function(){
     
 };
 
-var thisContact;
+var thisContact = new Contact;
